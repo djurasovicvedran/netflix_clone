0295ff0cb550de1d6b36d50c00445c47258199ea
djurasovicvedran <djurasovic.vedran@gmail.com>
2022-03-26 23:15:01 +0100

    Banner editing

---
+++ src/Row.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from "react"
+import axios from "./axios";
+import "./Row.css"
+const base_url = "https://image.tmdb.org/t/p/original/";
+// API KEY: "660b9724d86b68f233e7e75c8d32e5ff"
+
+
+
+function Row ({title, fetchUrl, isLargeRow}) {
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    
+
+    
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            <div className="row__posters">
+                {/* several row_posters */}
+                {movies.map(movie => (
+                    <img 
+                    key={movie.id}
+                    className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                    src={`${base_url}${isLargeRow? movie.poster_path : movie.backdrop_path }`} alt={movie.name} />
+                    ))}
+                    
+            </div>
+            { /* container -> posters */}
+        
+            
+        </div>
+    )
+}
+
+export default Row;
